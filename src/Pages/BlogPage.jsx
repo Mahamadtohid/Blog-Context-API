5d2058e522017e2c6bb9da547197f6481c5c905f
Mahamadtohid <naikwadimdtohid@gmail.com>
2025-07-10 23:51:17 +0530

    Routes Defining

--- src/Pages/BlogPage.jsx
+++ src/Pages/BlogPage.jsx
@@ -1,6 +1,27 @@
+import { useNavigate , useNavigation , useLocation } from "react-router-dom";
+// import useState from 'react';
+import {AppContext} from '../components/AppContext'
+import { BaseUrl } from "../components/BaseUrl";
+
+
 function BlogPage(){
+    const [blog , setBlog] = useState(null)
+    const [relatedBlogs , setRelatedBlogs] = useState([]);
+    const loaction = useLocation();
+    const navigate = useNavigation();
+    const [loading , setLoading] = useContext(AppContext)
+    const blogId = location.pathname.split("/").at(-1);
+
+    async function fetchRelatedBlogs(){
+        setLoading(true)
+
+        let url = `${BaseUrl}?blogId=${blogId}`
+        useEffetct()
+    }
 
     return (
+
+        
     
         <div>
 
--- src/Pages/CategoryPage.jsx
+++ src/Pages/CategoryPage.jsx
@@ -1,10 +1,33 @@
+import {useNavigate} from 'react-router-dom'
+import Header from '../components/Header'
+import {useLocation} from 'react-router-dom'
+import Blogs from '../components/Blogs'
+import Pagination  from '../components/Pagination'
+
+
 function CategoryPage(){
+    const navigation = useNavigate();
+    const location = useLocation();
+
+    const category = location.pathname.split("/").at(-1);
 
     return (
     
         <div>
+            <Header/>
+            <div>
+                <button
+                onClick={()=> navigation(-1)}
+                >
+                    Back
 
-
+                </button>
+                <h2>
+                    Blogs Tagged <span>{category}</span>
+                </h2>
+            </div>
+            <Blogs/>
+            <Pagination/>
 
         </div>
     
